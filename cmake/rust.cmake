c5020d5c3f841d5da9792c5a7d3f89edacbe1894
Luca Ciucci <luca.ciucci99@gmail.com>
2023-08-10 09:36:27 +0200

    Update rust.cmake

--- cmake/rust.cmake
+++ cmake/rust.cmake
@@ -8,7 +8,7 @@ the Rust library to a target.
 # You can modify this function by adding your own build types, but remember to add a
 # corresponding rust profile in the rust/Cargo.toml file.
 function(detect_rust_build_type _rust_build_type)
-    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug" OR NOT CMAKE_BUILD_TYPE)
         set(${_rust_build_type} "debug" PARENT_SCOPE)
     elseif(CMAKE_BUILD_TYPE STREQUAL "Release")
         set(${_rust_build_type} "release" PARENT_SCOPE)
@@ -27,14 +27,14 @@ endfunction()
 # You can modify this function by adding platform-specific suffixes.
 function(detect_profile_suffixes _dyn_lib_suffix _lib_suffix)
     if(WIN32)
-        set(${_dyn_lib_suffix} "dll" PARENT_SCOPE)
-        set(${_lib_suffix} "lib" PARENT_SCOPE)
+        set(${_dyn_lib_suffix} ".dll" PARENT_SCOPE)
+        set(${_lib_suffix} ".dll.lib" PARENT_SCOPE)
     elseif(UNIX)
-        set(${_dyn_lib_suffix} "so" PARENT_SCOPE)
-        set(${_lib_suffix} "a" PARENT_SCOPE)
+        set(${_dyn_lib_suffix} ".so" PARENT_SCOPE)
+        set(${_lib_suffix} ".a" PARENT_SCOPE)
     elseif(APPLE)
-        set(${_dyn_lib_suffix} "dylib" PARENT_SCOPE)
-        set(${_lib_suffix} "a" PARENT_SCOPE)
+        set(${_dyn_lib_suffix} ".dylib" PARENT_SCOPE)
+        set(${_lib_suffix} ".a" PARENT_SCOPE)
     else()
         message(FATAL_ERROR "Unknown platform")
     endif()
@@ -66,16 +66,16 @@ function(link_rust_library target)
     target_link_libraries(
         ${target}
         PRIVATE
-        "${CMAKE_CURRENT_LIST_DIR}/rust/target/${rust_build_type}/rust.${dyn_lib_suffix}.${lib_suffix}"
+        "${CMAKE_CURRENT_LIST_DIR}/rust/target/${rust_build_type}/rust${lib_suffix}"
     )
-    message(linked "${CMAKE_CURRENT_LIST_DIR}/rust/target/${rust_build_type}/rust.${dyn_lib_suffix}.${lib_suffix}")
+    message(linked "${CMAKE_CURRENT_LIST_DIR}/rust/target/${rust_build_type}/rust${lib_suffix}")
     add_dependencies(${target} build_rust_lib)
     target_include_directories(${target} PRIVATE "${CMAKE_CURRENT_LIST_DIR}/rust/bindings")
     add_custom_command(
         TARGET ${target} POST_BUILD
         COMMAND
             ${CMAKE_COMMAND} -E copy_if_different
-            "${CMAKE_CURRENT_LIST_DIR}/rust/target/${rust_build_type}/rust.${dyn_lib_suffix}"
+            "${CMAKE_CURRENT_LIST_DIR}/rust/target/${rust_build_type}/rust${dyn_lib_suffix}"
             $<TARGET_FILE_DIR:${target}>
     )
 endfunction()
