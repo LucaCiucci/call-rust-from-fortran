c3dccd80617b20ae6fbcaee0a35ba43f60daa688
Luca Ciucci <luca.ciucci99@gmail.com>
2023-08-12 08:43:47 +0200

    remove test module

--- src/hello.f90
+++ src/hello.f90
@@ -2,7 +2,6 @@ program hello_world
     ! We need to use the ISO_C_BINDING module to declare the types compatible with C
     use iso_c_binding
     use rust
-    use some_module
 
     implicit none
 
@@ -15,6 +14,5 @@ program hello_world
     call hello_from_rust()
     print *, "2 + 3 = ", rustacean_sum(2, 3)
     call display_rustacean_struct(s)
-    call hello()
 
 end program hello_world
--- src/some_module.f90
+++
@@ -1,9 +0,0 @@
-module some_module
-    implicit none
-
-    contains
-
-    subroutine hello()
-        print *, "Hello, world!"
-    end subroutine hello
-end module some_module
